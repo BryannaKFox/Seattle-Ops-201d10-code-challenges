fa2022ea707c0ac046e680bc959e704eef327735
BryannaKFox <bfoxxxy1@gmail.com>
2023-10-24 06:38:27 +0000

    challenge 02

---
+++ helloworld.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# Script Name:                  ops201d10-challenge02-my-first-bash-script
+# Author:                       Bryanna Fox
+# Date of latest revision:      10/23/2023
+# Purpose:                      Print a string to the terminal
+
+# Declaration of variables
+
+# Declaration of functions
+
+
+# Main
+
+
+
+# End
