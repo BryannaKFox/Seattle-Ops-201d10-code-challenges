ef177406017968759277eb8e0ee0cab51ee84a2f
BryannaKFox <bfoxxxy1@gmail.com>
2023-10-25 03:22:01 +0000

    Challenge03_shell_functions

---
+++ Challenge03_shell_functions.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+# Script:                       ops201d10-challenge03-shell-functions
+# Author:                       Bryanna Fox
+# Date of latest revision:      10/24/2023
+# Purpose:                      Write a function that prints the login history of users on the computer.
+
+# Declaration of functions
+
+# Basic function
+print_something () {
+    last
+  echo "This is the login history"
+}
+
+# Main
+
+for ((i=0; i<3; i++)); do
+    print_login_history
+    done
+
+# End
