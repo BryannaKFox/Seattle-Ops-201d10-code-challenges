2e2b0761273ff102f25077da5238344b38b6b6d8
BryannaKFox <bfoxxxy1@gmail.com>
2023-10-26 06:37:07 +0000

    arrays

--- challenge04_arrays.sh
+++ challenge04_arrays.sh
@@ -15,10 +15,10 @@ mkdir "ohmy"
 saying=("lion" "tiger" "bear" "ohmy")
 
 # Accessing elements in the array
-echo "The first saying: ${saying[lion]}"
-echo "The second saying: ${saying[tiger]}"
-echo "The third saying: ${saying[bear]}"
-echo "The fourth saying: ${saying[ohmy]}"
+echo "The first saying: ${saying[0]}"
+echo "The second saying: ${saying[1]}"
+echo "The third saying: ${saying[2]}"
+echo "The fourth saying: ${saying[3]}"
 
 # Append a new saying to the array
 new_saying="there's no place like home"
