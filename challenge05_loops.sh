8658bedaa4d0e4d4db9fd70d5bc2d41a61003467
BryannaKFox <bfoxxxy1@gmail.com>
2023-10-27 04:11:37 +0000

    loops

---
+++ challenge05_loops.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+# Script:                     ops201d10-challenge05-loops  
+# Author:                     Bryanna Fox  
+# Date of latest revision:    10/26/2023  
+# Purpose:                    Write a script using a loop  
+
+# Basic for loop
+#!/bin/bash
+
+while true; do
+    # Display running processes
+    echo "Running processes:"
+    ps aux
+
+    # Ask the user for a PID
+    read -p "Enter the PID of the process to kill (Ctrl+C to exit): " pid
+
+    # Check if the PID is valid
+    if [[ "$pid" =~ ^[0-9]+$ ]]; then
+        # Kill the process with the given PID
+        kill -9 "$pid"
+        echo "Process with PID $pid killed."
+    else
+        echo "Invalid PID. Please enter a valid numeric PID."
+    fi
+done
+
+
+
+# End
