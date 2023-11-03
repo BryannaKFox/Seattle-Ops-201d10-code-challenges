0ee1153377e597e0e947f52c075669d078367d06
BryannaKFox <146906807+BryannaKFox@users.noreply.github.com>
2023-11-02 20:55:29 -0700

    Update challenge09_LogRetrevial.ps1

--- challenge09_LogRetrevial.ps1
+++ challenge09_LogRetrevial.ps1
@@ -12,10 +12,17 @@ $events | Format-Table -Autosize | Out-File -FilePath $outputFilePath -Force
 Write-Host "Events from the last 24 hours have been exported to: $outputFilePath"
 
 #Output all "error" type events from the System event log to a file on your desktop named errors.txt.
-
+$events = Get-WinEvent -LogName System -FilterXPath "*[System[Level=2]]" -ErrorAction Stop
+$errorEvents = $events | Where-Object { $_.LevelDisplayName -eq 'Error' }
+$outputFilePath = [System.IO.Path]::Combine($env:USERPROFILE, 'Desktop\errors.txt')
+$errorEvents | Out-File -FilePath $outputFilePath -Force
+Write-Host "Error events from the System log have been exported to: $outputFilePath"
 
 #Print to the screen all events with ID of 16 from the System event log. 
+Get-WinEvent -LogName 'System' -FilterHashtable @{ ID = 16 } -ErrorAction Stop | ForEach-Object { Write-Host "Event ID: $($_.Id) `nTime Created: $($_.TimeCreated) `nMessage: $($_.Message) `n------------------------" } } catch { Write-Host "Error: $_" 
 
 #Print to the screen the most recent 20 entries from the System event log.
+Get-WinEvent -LogName 'System' -MaxEvents 20 -ErrorAction Stop | ForEach-Object { Write-Host "Event ID: $($_.Id) `nTime Created: $($_.TimeCreated) `nMessage: $($_.Message) `n------------------------" } } catch { Write-Host "Error: $_"
 
 #Print to the screen all sources of the 500 most recent entries in the System event log. Ensure that the full lines are displayed (get rid of the ... and show the entire text.)
+Get-WinEvent -LogName 'System' -MaxEvents 500 -ErrorAction Stop | Select-Object -ExpandProperty ProviderName } catch { Write-Host "Error: $_"
